13c5b7a0ca384d61a51d2e08808e7fc486c8fa0e
ZIMC-AYZI <zimcgang@gmail.com>
2024-06-24 03:35:41 +0200

    build 1.2 added base href

--- docs/browser/chunk-DVQMR7RM.js
+++
@@ -1,1 +0,0 @@
-import{Ob as r,Vb as p,la as o,wb as a,xb as i}from"./chunk-Y3JPPDWQ.js";var c=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-not-found-page"]],standalone:!0,features:[p],decls:2,vars:0,template:function(n,d){n&1&&(a(0,"p"),r(1,"not-found-page works!"),i())}});let t=e;return t})();export{c as NotFoundPageComponent};
